02ea55fbc10648fd34deaf0380c996ea6ff7ec39
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-25 14:02:47 +0530

    Setting up adapter in MainActivity done commit

--- app/src/main/java/com/example/trackcrypto/MainActivity.java
+++ app/src/main/java/com/example/trackcrypto/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.trackcrypto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     private EditText searchEdit;
     private RecyclerView recyclerView;
     private ProgressBar loading;
+    private ArrayList<Model_RV_currency> model_rv_currencyArrayList;
+    private Currency_RV_Adapter currency_rv_adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
         searchEdit = findViewById(R.id.Search);
         recyclerView = findViewById(R.id.currencies);
         loading = findViewById(R.id.progress);
+
+        model_rv_currencyArrayList = new ArrayList<>();
+        currency_rv_adapter = new Currency_RV_Adapter(model_rv_currencyArrayList , this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(currency_rv_adapter);
     }
 }
