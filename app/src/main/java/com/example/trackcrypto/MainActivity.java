91c9344ecf6e965d2b12dd25a49d4b3d7f808651
Suraj Kumar Modi <76468931+skmodi649@users.noreply.github.com>
2021-11-25 12:52:20 +0530

    list item xml for adapter done commit

--- app/src/main/java/com/example/trackcrypto/MainActivity.java
+++ app/src/main/java/com/example/trackcrypto/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.trackcrypto;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText searchEdit;
+    private RecyclerView recyclerView;
+    private ProgressBar loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        searchEdit = findViewById(R.id.Search);
+        recyclerView = findViewById(R.id.currencies);
+        loading = findViewById(R.id.progress);
     }
 }
